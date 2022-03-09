e38f01f02f610bc1aa19d915eba16981bd3854a7
Mikhail Profatsky <mixail.rostov04@gmail.com>
2022-03-09 15:25:07 +0300

    Раздаточный материал №62

---
+++ module/main.py
@@ -0,0 +1,15 @@
+import module
+
+module.get_set()
+
+module.get_txt()
+
+print(module.list_doc.doc)
+
+print(module.file_doc.doc)
+
+print(module.get_binary())
+
+print(module.file_doc.doc)
+
+print(dir(module))
